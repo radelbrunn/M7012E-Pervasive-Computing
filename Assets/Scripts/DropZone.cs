c55ad1280fbd9132a0413d2c8b0861c84fced389
GentlemanPalm <gentleman.palm@gmail.com>
2019-01-08 13:17:50 +0100

    Update DropZone.cs
    Made clearer comments and altered those from the unity examples

--- Assets/Scripts/DropZone.cs
+++ Assets/Scripts/DropZone.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DropZone : MonoBehaviour {
-    public LayerMask m_LayerMask;
+    public LayerMask m_LayerMask; // Allows filtering collisions on layers. Set the appropriate layers in the unity editor. 
 
-    public SumListener listener;
+    public SumListener listener; // The listener of sum changed. Public so it may be set from the editor. 
 
     private int sum = 0;
 
@@ -21,20 +21,19 @@ public class DropZone : MonoBehaviour {
 
     void MyCollisions()
     {
-        // Use the OverlapBox to detect if there are any other colliders within this box area.
-        // Use the GameObject's centre, half the size (as a radius) and rotation. This creates an invisible box around your GameObject.
+        // Create overlap box around the center of the object. It should have the sides of the scale of the object.
         Collider[] hitColliders = Physics.OverlapBox(gameObject.transform.position, transform.localScale / 2, Quaternion.identity, m_LayerMask);
         int i = 0;
         // Check when there is a new collider coming into contact with the box
          int s = 0;
-        while (i < hitColliders.Length)
-        {
+        while (i < hitColliders.Length) // Check each and every collider
+        { // Iterate only over those implementing iValue. This is to avoid scanning the Players hands.
 			foreach (iValue vs in hitColliders[i].gameObject.GetComponents(typeof(iValue))) {
-                s += vs.GetValue();
+                s += vs.GetValue(); // Sum up all values from objects in the area. 
 			}
             i++;
         }
-        if (s != sum) {
+        if (s != sum) { // Only updates the listener if there has been a change of sum.
                 int old = sum;
                 sum = s;
                 if (listener != null) {
@@ -45,11 +44,10 @@ public class DropZone : MonoBehaviour {
             }
     }
 
-    //Draw the Box Overlap as a gizmo to show where it currently is testing. Click the Gizmos button to see this
+    // Use gizmos to allow viewing the bounds of the drop zone in scene mode, while remaining hidden in play mode.
     void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
-        //Check that it is being run in Play Mode, so it doesn't try to draw this in Editor mode
         Gizmos.DrawWireCube(transform.position, transform.localScale);
     }
 }
