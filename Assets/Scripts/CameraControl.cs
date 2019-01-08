4ddb5c463d5becb1d05577e61105d5db32c9e6ab
GentlemanPalm <gentleman.palm@gmail.com>
2019-01-08 13:55:53 +0100

    Update CameraControl.cs

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -2,13 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// Standard unity boilerplate for camera control
+// Makes for a camera that follows the player from a set distance
+// All credits to the built-in unity tutorials for this code
 public class CameraControl : MonoBehaviour {
 
 	public GameObject player;
 
 	private Vector3 offset;
 
-	// Use this for initialization
 	void Start () {
 		offset = transform.position - player.transform.position;
 	}
