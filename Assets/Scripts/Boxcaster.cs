6ff02d3ae99e2c44b10c19d8dc1993db07c701d2
GentlemanPalm <gentleman.palm@gmail.com>
2019-01-08 13:37:29 +0100

    Update Boxcaster.cs

--- Assets/Scripts/Boxcaster.cs
+++ Assets/Scripts/Boxcaster.cs
@@ -1,7 +1,11 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+ * This class is the complex boxcaster made for scanning letters for the spelling game.
+ * It relies on boxcasting a very thin block and order collisions by distance. 
+ * */
 public class Boxcaster : MonoBehaviour {
 
     private Vector3 size;
@@ -9,24 +13,31 @@ public class Boxcaster : MonoBehaviour {
     public string content;
     public LayerMask m_LayerMask;
 
-    private float width = 0.02f;
+    private float width = 0.02f; // Want _really_ thin blocks for casting to increase accuracy.
 
 
 	// Use this for initialization
 	void Start () {
-        maxDistance = gameObject.transform.localScale.z;
+        maxDistance = gameObject.transform.localScale.z; // We want to cast in the Z-direction
         size = new Vector3(gameObject.transform.localScale.x, gameObject.transform.localScale.y, width);
 	}
 	
 	// Update is called once per frame
 	void Update () {
+	
+	// create the very thin sheet that will be used for casting.
         size = new Vector3(gameObject.transform.localScale.x/2, gameObject.transform.localScale.y/2, width);
-        maxDistance = gameObject.transform.localScale.z;
-        Vector3 origin = gameObject.transform.position - (gameObject.transform.forward * maxDistance / 2);
+        
+	// Go along the entire 'Z' component of this object
+	maxDistance = gameObject.transform.localScale.z;
+        
+	// Set the origin at the 'wall' of the box at it's "negative Z" direction
+	Vector3 origin = gameObject.transform.position - (gameObject.transform.forward * maxDistance / 2);
         Vector3 direction = gameObject.transform.forward;
 
         size = size;
-
+	
+	// Do the raycast 
         RaycastHit[] hits = Physics.BoxCastAll(
             origin,
             size,
@@ -34,12 +45,13 @@ public class Boxcaster : MonoBehaviour {
             gameObject.transform.localRotation,
             maxDistance,
             m_LayerMask);
-
+		
+	// Sort along distance.
         System.Array.Sort(hits, (x, y) => x.distance.CompareTo(y.distance));
 
         string result = "";
         content = "";
-        foreach (RaycastHit rhit in hits) {
+        foreach (RaycastHit rhit in hits) { // Build a string of all letters in the objects hit by the raycast.
             ILetters[] letters = rhit.collider.gameObject.GetComponents<ILetters>();
             foreach (ILetters letter in letters) {
                 result += " " + letter.GetLetters();
@@ -54,6 +66,8 @@ public class Boxcaster : MonoBehaviour {
 
     private void OnDrawGizmos()
     {
+	// This is some pretty crappy code duplication of the scanning done in Update()
+	// This code illustrates the raycast for the editor.
         maxDistance = gameObject.transform.localScale.z;
         size = new Vector3(gameObject.transform.localScale.x, gameObject.transform.localScale.y, width);
         size = gameObject.transform.localRotation * size;
